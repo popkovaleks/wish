41ba69dbb5bee07d2413f072ff2432ade14dc26e
popkovaleks <popkov.aleks.v@yandex.ru>
2025-07-12 09:32:23 +0500

    Dto for user response

--- src/main/java/com/example/accessing_data_mysql/controllers/MainController.java
+++ src/main/java/com/example/accessing_data_mysql/controllers/MainController.java
@@ -1,11 +1,15 @@
 package com.example.accessing_data_mysql.controllers;
 
 
+import com.example.accessing_data_mysql.dto.UserResponseDto;
 import com.example.accessing_data_mysql.models.User;
 import com.example.accessing_data_mysql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping(path="/demo")
@@ -27,7 +31,22 @@ public class MainController {
     }
 
     @GetMapping(path="/all")
-    public @ResponseBody Iterable<User> getAllUsers() {
-        return userRepository.findAll();
+    public @ResponseBody List<UserResponseDto> getAllUsers() {
+        Iterable<User> users = userRepository.findAll();
+        List<UserResponseDto> userDtos = new ArrayList<>();
+        for(User user : users){
+            UserResponseDto userDto = toDto(user);
+            userDtos.add(userDto);
+
+        }
+        return userDtos;
+    }
+
+    public static UserResponseDto toDto(User user){
+        UserResponseDto dto = new UserResponseDto();
+        dto.setId(user.getId());
+        dto.setName(user.getName());
+        dto.setEmail(user.getEmail());
+        return dto;
     }
 }
