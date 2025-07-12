706d9781ae90414cd3f9a4713fbecf9b5c192eb6
popkovaleks <popkov.aleks.v@yandex.ru>
2025-07-12 15:21:19 +0500

    Get item by userId

--- src/main/java/com/example/accessing_data_mysql/mappers/ItemMapper.java
+++ src/main/java/com/example/accessing_data_mysql/mappers/ItemMapper.java
@@ -1,16 +1,20 @@
 package com.example.accessing_data_mysql.mappers;
 
 import com.example.accessing_data_mysql.dto.ItemCreateDto;
+import com.example.accessing_data_mysql.dto.ItemDto;
 import com.example.accessing_data_mysql.models.Item;
 import com.example.accessing_data_mysql.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+
+@Component
 public class ItemMapper {
 
     @Autowired
-    private static UserRepository userRepository;
+    private UserRepository userRepository;
 
-    public static Item toEntity(ItemCreateDto itemDto){
+    public Item toEntity(ItemCreateDto itemDto){
         Item item = new Item();
 
         item.setUser(userRepository.findById(itemDto.getUserId()).get());
@@ -20,10 +24,19 @@ public class ItemMapper {
         return item;
     }
 
-    public static ItemCreateDto toDto(Item item){
+    public  ItemCreateDto toDto(Item item){
         ItemCreateDto itemDto = new ItemCreateDto();
 
         itemDto.setUserId(item.getUser().getId());
+        itemDto.setLink(item.getLink());
+        itemDto.setPrice(item.getPrice());
+        itemDto.setName(item.getName());
+        return itemDto;
+    }
+
+    public  ItemDto toResponseDto(Item item){
+        ItemDto itemDto = new ItemDto();
+
         itemDto.setLink(item.getLink());
         itemDto.setPrice(item.getPrice());
         itemDto.setName(item.getName());
